f1368aca48cf93ec1d3a919cee8d17c6147b1c6b
othman <othman.boutab@madagence.com>
2023-08-11 18:02:01 +0200

    add comment

--- js/utils/cards.js
+++ js/utils/cards.js
@@ -1,63 +1,78 @@
 export function createCards(recipes) {
-
+    // Récupération du conteneur des cartes
     const cardsContainer = document.querySelector(".cards-container");
 
+    // Parcours de chaque recette
     for (let i = 0; i < recipes.length; i++) {
-        const listItem = document.createElement('div');
-        listItem.setAttribute('class', 'listItem')
+        const recipe = recipes[i];
 
+        // Création de la structure de la carte
+        const listItem = document.createElement('div');
+        listItem.setAttribute('class', 'listItem');
 
         const card = document.createElement('div');
-        card.setAttribute('class', 'card')
-        listItem.append(card)
-
+        card.setAttribute('class', 'card');
+        listItem.append(card);
 
         const image = document.createElement('div');
-        image.setAttribute('class', 'image')
-        card.append(image)
+        image.setAttribute('class', 'image');
+        card.append(image);
 
         const detail = document.createElement('div');
-        detail.setAttribute('class', 'detail')
-        card.append(detail)
+        detail.setAttribute('class', 'detail');
+        card.append(detail);
 
         const nameTime = document.createElement('div');
-        nameTime.setAttribute('class', 'name-time')
-        detail.append(nameTime)
+        nameTime.setAttribute('class', 'name-time');
+        detail.append(nameTime);
 
+        // Affichage du nom de la recette
         const name = document.createElement('span');
-        name.setAttribute('class', 'name')
-        name.innerText = recipes[i]?.name
-        nameTime.append(name)
+        name.setAttribute('class', 'name');
+        name.innerText = recipe?.name;
+        nameTime.append(name);
 
+        // Affichage du temps de préparation
         const timeContainer = document.createElement('span');
-        timeContainer.setAttribute('class', 'timeContainer')
-        timeContainer.innerHTML = `<span class="time-container"><i class="fa-regular fa-clock"></i><span class="time">${recipes[i]?.time}</span>min</span>`
-        nameTime.append(timeContainer)
+        timeContainer.setAttribute('class', 'timeContainer');
+        timeContainer.innerHTML = `<span class="time-container"><i class="fa-regular fa-clock"></i><span class="time">${recipe?.time}</span>min</span>`;
+        nameTime.append(timeContainer);
 
+        // Affichage des ingrédients
         const ingredientsDesc = document.createElement('div');
-        ingredientsDesc.setAttribute('class', 'ingredients-desc')
-        detail.append(ingredientsDesc)
+        ingredientsDesc.setAttribute('class', 'ingredients-desc');
+        detail.append(ingredientsDesc);
 
         const ingredients = document.createElement('span');
-        ingredients.setAttribute('class', 'ingredients')
+        ingredients.setAttribute('class', 'ingredients');
 
-        recipes[i]?.ingredients?.forEach(ingredient => {
-            ingredients.innerHTML += `
-        <span class="ingredient">
-        <span class="ingredient">${ingredient?.ingredient}</span>
-        ${ingredient?.quantity ? `:<span class="quantity">${ingredient?.quantity}</span>` : ''}
-        ${ingredient?.unit ? `<span class="unit">${ingredient?.unit}</span>` : ''}
-    </span>
-            `
-        })
-        ingredientsDesc.append(ingredients)
+        // Parcours de chaque ingrédient dans la recette
+        for (let j = 0; j < recipe?.ingredients?.length; j++) {
+            const ingredient = recipe.ingredients[j];
 
+            // Construction de la balise HTML pour l'ingrédient
+            const ingredientElement = document.createElement('span');
+            ingredientElement.setAttribute('class', 'ingredient');
+
+            // Ajout de l'ingrédient avec sa quantité et unité (si disponibles)
+            ingredientElement.innerHTML = `
+                <span class="ingredient">${ingredient?.ingredient}</span>
+                ${ingredient?.quantity ? `:<span class="quantity">${ingredient?.quantity}</span>` : ''}
+                ${ingredient?.unit ? `<span class="unit">${ingredient?.unit}</span>` : ''}
+            `;
+
+            ingredients.appendChild(ingredientElement);
+        }
+
+        ingredientsDesc.append(ingredients);
+
+        // Affichage de la description de la recette
         const description = document.createElement('span');
-        description.setAttribute('class', 'description')
-        description.innerText = recipes[i]?.description
-        ingredientsDesc.append(description)
+        description.setAttribute('class', 'description');
+        description.innerText = recipe?.description;
+        ingredientsDesc.append(description);
 
-        cardsContainer.append(listItem)
+        // Ajout de la carte au conteneur
+        cardsContainer.append(listItem);
     }
-
 }
--- js/utils/filter.js
+++ js/utils/filter.js
@@ -2,11 +2,14 @@ import { createCards } from '../utils/cards.js'
 
 
 export function buildFilter(name, placeholder, values, parent) {
+    // Créer un élément de sélection (liste déroulante)
     let select = document.createElement('select')
 
+    // Définir le nom et l'ID de l'élément de sélection
     select.name = name;
     select.id = name
 
+    // Créer une option de placeholder désactivée et sélectionnée par défaut
     let placeHolderOption = document.createElement("option");
     placeHolderOption.value = "";
     placeHolderOption.text = placeholder;
@@ -14,8 +17,10 @@ export function buildFilter(name, placeholder, values, parent) {
     placeHolderOption.selected = true;
     placeHolderOption.hidden = true;
 
+    // Ajouter l'option de placeholder à l'élément de sélection
     select.appendChild(placeHolderOption)
 
+    // Parcourir les valeurs et créer une option pour chaque valeur
     for (const val of values) {
         let option = document.createElement("option");
         option.value = val;
@@ -24,46 +29,59 @@ export function buildFilter(name, placeholder, values, parent) {
         select.appendChild(option);
     }
 
+    // Ajouter l'élément de sélection au parent spécifié
     parent.append(select)
 }
 
-
 export function filterAndDisplayRecipes(filteredRecipes) {
     const cardsContainer = document.querySelector(".cards-container");
+
+    // Effacer le contenu actuel du conteneur de cartes
     cardsContainer.innerHTML = "";
+
+    // Appeler la fonction createCards pour afficher les recettes filtrées dans le conteneur
     createCards(filteredRecipes);
 }
 
 export function recipeIngredientsInclude(recipe, searchTerm) {
     let j = 0;
     while (j < recipe.ingredients.length) {
+        // Vérifie si le terme de recherche est inclus dans l'ingrédient actuel (en minuscules)
         if (recipe.ingredients[j].ingredient.toLowerCase().includes(searchTerm)) {
-            return true;
+            return true; // Si trouvé, renvoie true
         }
         j++;
     }
-    return false;
+    return false; // Si le terme n'a pas été trouvé dans tous les ingrédients, renvoie false
 }
 
 
 export function utensilsIngredientsInclude(recipe, searchTerm) {
     let j = 0;
     while (j < recipe.ustensils.length) {
+        // Vérifie si le terme de recherche est inclus dans l'ustensile actuel (en minuscules)
         if (recipe.ustensils[j].toLowerCase().includes(searchTerm)) {
-            return true;
+            return true; // Si trouvé, renvoie true
         }
         j++;
     }
-    return false;
+    return false; // Si le terme n'a pas été trouvé dans tous les ustensiles, renvoie false
 }
 
 export function addButton(container, text, color, onClick) {
+    // Crée un élément bouton
     const button = document.createElement("button");
-    button.textContent = text;
-    button.style.backgroundColor = color;
-    button.addEventListener("click", onClick);
+    button.textContent = text; // Définit le texte du bouton
+    button.style.backgroundColor = color; // Définit la couleur d'arrière-plan du bouton
+    button.addEventListener("click", onClick); // Ajoute l'événement clic au bouton
+
+    // Crée un élément icône de suppression
     const deleteIcon = document.createElement('span')
     deleteIcon.innerHTML = '<i class="fa-regular fa-circle-xmark"></i>'
-    button.appendChild(deleteIcon)
+
+    // Ajoute l'icône de suppression au bouton
+    button.appendChild(deleteIcon);
+
+    // Ajoute le bouton au conteneur spécifié
     container.appendChild(button);
 }
