7cf2fc5f126057568c5ecdb67ed7b9bfb36b11b1
othman <othman.boutab@madagence.com>
2023-12-13 09:58:52 +0100

    update search value

--- js/utils/search.js
+++ js/utils/search.js
@@ -3,6 +3,6 @@ import { updateFilteredRecipes } from "./filtre.js";
 export function setupSearch() {
   const searchInput = document.getElementById("search-bar");
   searchInput.addEventListener("input", () => {
-    updateFilteredRecipes(searchInput.value);
+    updateFilteredRecipes(searchInput.value.toLowerCase());
   });
 }
